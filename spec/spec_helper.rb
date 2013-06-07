905a9681e3de073bfa66dead5554c1e00486f865
Dan Kubb <dan.kubb@gmail.com>
2013-06-06 23:40:25 -0700

    Extract helper method into spec support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,9 +18,15 @@ end
 require 'sql'
 require 'devtools/spec_helper'
 
+# require spec support files and shared behavior
+Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each do |file|
+  require file
+end
+
 RSpec.configure do |config|
   config.include(SQL::NodeHelper)
   config.extend(SQL::NodeHelper)
+  config.extend(EmitterSpecHelper)
   config.expect_with :rspec do |expect_with|
     expect_with.syntax = :expect
   end
---
+++ spec/support/emitter_helper.rb
@@ -0,0 +1,18 @@
+module EmitterSpecHelper
+
+  # Create an example that the expected SQL is generated
+  #
+  # @param [SQL::AST::Node] node
+  # @param [String] expectation
+  #
+  # @return [undefined]
+  #
+  # @api public
+  def assert_generates(node, expectation)
+    it "generates #{node.type} correctly" do
+      SQL::Generator::Emitter.visit(node, buffer)
+      expect(buffer.content).to eql(expectation)
+    end
+  end
+
+end # EmitterSpecHelper
