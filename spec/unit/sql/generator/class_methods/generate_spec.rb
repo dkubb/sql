f75fa4352223cb1ce7bd1e49c9f0669b630807b3
Dan Kubb <dan.kubb@gmail.com>
2013-06-07 00:16:23 -0700

    Refactor SQL::Generator.generate spec to be simpler

--- spec/unit/sql/generator/class_methods/generate_spec.rb
+++ spec/unit/sql/generator/class_methods/generate_spec.rb
@@ -5,23 +5,10 @@ require 'spec_helper'
 describe SQL::Generator do
   subject { object.generate(node) }
 
-  let(:object) { described_class               }
-  let(:node)   { stub('node')                  }
-  let(:buffer) { mock('buffer').as_null_object }
+  let(:object) { described_class }
+  let(:node)   { s(:true)        }
 
-  before do
-    SQL::Generator::Buffer.stub(:new => buffer)
-    SQL::Generator::Emitter.stub(:visit)
-  end
-
-  it 'returns the buffer content' do
-    content = stub('content')
-    buffer.should_receive(:content).and_return(content)
-    expect(subject).to be(content)
-  end
-
-  it 'visits the ast' do
-    SQL::Generator::Emitter.should_receive(:visit).with(node, buffer)
-    subject
+  it 'generates the expected SQL' do
+    expect(subject).to eql('TRUE')
   end
 end
